c4f8786cb795af1d0fcb3ceac8139e75dcb5baec
CThompson01 <cthompson10547@gmail.com>
2023-11-07 00:12:31 -0600

    Add Mood Slider
    Added the mood slider to the create post popup. Changed the lorem ipsum text on the popup to be an edit text input. Also hid the create post popup by default.

--- Project2/app/src/main/java/com/example/project2/Activities/HomeActivity.java
+++ Project2/app/src/main/java/com/example/project2/Activities/HomeActivity.java
@@ -6,12 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.project2.Database.MoodPost;
 import com.example.project2.R;
+import com.google.android.material.slider.Slider;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -24,6 +26,9 @@ public class HomeActivity extends AppCompatActivity {
         Button closeCreatePostPopupButton = findViewById(R.id.close_create_post);
         Button postCreatedPost = findViewById(R.id.post);
         LinearLayout createPostPopup = findViewById(R.id.create_post_popup);
+
+        EditText createPostEntryInput = ((EditText) findViewById(R.id.mood_entry));
+        Slider createPostMoodInput = findViewById(R.id.mood_rating);
 
         createPostButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,11 +49,17 @@ public class HomeActivity extends AppCompatActivity {
             public void onClick(View v) {
                 int postId = -1; // TODO get post id from server (ideally just make it count up)
                 int posterId = -1; // TODO get poster id from server (requires login)
-                String moodEntry = ((TextView) findViewById(R.id.mood_entry)).getText().toString();
-                int moodRating = -1; // TODO add mood rating slider
+                String moodEntry = createPostEntryInput.getText().toString();
+                int moodRating = (int) createPostMoodInput.getValue();
                 MoodPost post = new MoodPost(postId, posterId, moodEntry, moodRating);
 
+                // Reset the edit text
+                createPostEntryInput.setText("");
+
                 // TODO send post to the database
+                System.out.printf("Post Info:\n   PostId: %d\n   PosterId: %d\n   " +
+                        "Entry: %s\n   Rating: %d\n", post.getPostId(), post.getPosterId(),
+                        post.getMoodEntry(), post.getMoodRating());
                 createPostPopup.setVisibility(View.GONE);
             }
         });
