38e0dc4d15b1a9746e4ff30573b06934a54b4ff6
Thompson <cthomp01@iastate.edu>
2023-12-08 13:49:24 -0600

    Add Changing Username
    Successfully implement changing usernames.

--- Project2/app/src/main/java/com/example/project2/Activities/ProfileSettingsActivity.java
+++ Project2/app/src/main/java/com/example/project2/Activities/ProfileSettingsActivity.java
@@ -7,9 +7,11 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -51,6 +53,8 @@ public class ProfileSettingsActivity extends AppCompatActivity {
 
         // Init alert builder
         AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+
+        // Build delete account alert
         alertBuilder.setMessage("Are you sure you want to delete your account?\n" +
                         "(Once an account is deleted, there is no way to recover it)")
                 .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
@@ -77,10 +81,36 @@ public class ProfileSettingsActivity extends AppCompatActivity {
                 });
         Dialog deleteAccountAlert = alertBuilder.create();
 
+        // Build change username alert
+        final EditText userInput = new EditText(this);
+        userInput.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        alertBuilder.setView(userInput);
+        alertBuilder.setMessage("Please enter a new username.")
+                .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        updateUsernameButton(userInput.getText().toString(), mFirestore);
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+        Dialog changeUsernameAlert = alertBuilder.create();
+
         // Init layout references
         Button signOutButton = findViewById(R.id.sign_out);
         Button deleteAccountButton = findViewById(R.id.delete_account);
         Button backButton = findViewById(R.id.back_button);
+        Button changeUsernameButton = findViewById(R.id.change_username_button);
+
+        changeUsernameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeUsernameAlert.show();
+            }
+        });
 
         signOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -145,6 +175,50 @@ public class ProfileSettingsActivity extends AppCompatActivity {
 
                 // Update current user friends list
                 currentUser.addFriend(friendUid);
+
+                transaction.set(userRef, currentUser);
+                return null;
+            }
+        });
+    }
+
+    private void updateUsernameButton(String username, FirebaseFirestore mFirestore) {
+        usersCollection.whereEqualTo("uid", user.getUid()).get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            QuerySnapshot documentSnapshot = task.getResult();
+                            List<DocumentSnapshot> documentSnapshotList = documentSnapshot.getDocuments();
+                            if (documentSnapshotList.size() > 0) {
+                                DocumentReference docRef = usersCollection.document(documentSnapshotList.get(0).getId());
+                                Task<Void> tsk = updateUsername(username, docRef, mFirestore);
+                                if (tsk.isSuccessful()) {
+                                    System.out.println("updatingUsername: taskSuccess");
+                                } else {
+                                    System.out.println("updatingUsername: taskFailed");
+                                }
+                            }
+                        }
+                    }
+                });
+    }
+
+    private Task<Void> updateUsername(String username, DocumentReference userRef, FirebaseFirestore mFirestore) {
+        // Push to database
+        return mFirestore.runTransaction(new Transaction.Function<Void>() {
+            @Nullable
+            @Override
+            public Void apply(@NonNull Transaction transaction) throws FirebaseFirestoreException {
+                DocumentSnapshot currUserDoc = transaction.get(userRef);
+                String uid = currUserDoc.get("uid").toString();
+                List<String> friends = (List<String>) currUserDoc.get("friends");
+
+                // Recreate the user
+                User currentUser = new User(username, uid);
+                for (String friend : friends) {
+                    currentUser.addFriend(friend);
+                }
 
                 transaction.set(userRef, currentUser);
                 return null;
